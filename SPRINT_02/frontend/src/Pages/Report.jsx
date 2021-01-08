1e577c16f0b955b1e3cdefd6048ed8e47b4b5922
Nayara <nayara.lopes2007@gmail.com>
2021-01-08 20:54:53 -0300

    Criando_pg_relatorio

--- SPRINT_02/frontend/src/Pages/Report.jsx
+++ SPRINT_02/frontend/src/Pages/Report.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import CardReport from '../Components/CardReport/index';
 
 export default function Report() {
     return (
-        <div>
-            <h1>Report</h1>
-            
-        </div>
+        <div className="section-3">
+              <h2>Selecione o mês</h2>
+                <div className="box">
+                  
+                    <section className="categories-container">
+                        <CardReport title="Janeiro" />
+                        <CardReport title="Fevereiro" />
+                        <CardReport title="Marco" />
+                        <CardReport title="Abril"  />
+                        <CardReport title="Maio" />
+                        <CardReport title="Junho" />
+                        <CardReport title="Julho"  />
+                        <CardReport title="Agosto" />
+                        <CardReport title="Setembro" />
+                        <CardReport title="Outubro" />
+                        <CardReport title="Novembro" />
+                        <CardReport title="Dezembro" />
+                        
+                    </section>
+                    
+                </div>
+            </div>
     )
 }
