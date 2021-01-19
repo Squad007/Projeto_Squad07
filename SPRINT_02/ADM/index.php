d243f638ff0d34069a667034a736d0ebbe78619f
CristianoSantan <cristiano.mer@gmail.com>
2021-01-18 21:49:21 -0300

    menu e login

--- SPRINT_02/ADM/index.php
+++ SPRINT_02/ADM/index.php
@@ -4,6 +4,24 @@
 <head>
   <?php require_once('bootstrap.html') ?>
   <title>ADM SOS - Login</title>
+
+  <style>
+    @media(max-width: 600px) {
+      .logo {
+        display: none;
+      }
+      
+
+      .login {
+        width: 50%;
+        font-size: 20px;
+        text-align: center;
+        right: 10%;
+      }
+
+
+    }
+  </style>
 </head>
 
 <body style="background: rgb(18,18,20);
@@ -13,25 +31,20 @@ background: linear-gradient(90deg, rgba(18,18,20,1) 0%, rgba(18,18,20,1) 50%, rg
 
 
     <section class="col d-flex justify-content-center align-items-center" style="width: 50vh; height: 100vh;">
-      <div style="background-image: url(./img/logo.svg); width: 60%; height: 80vh; background-position: center; background-size:contain; background-repeat:no-repeat"></div>
+      <div class="logo" style="background-image: url(./img/logo.svg); width: 60%; height: 80vh; background-position: center; background-size:contain; background-repeat:no-repeat"></div>
     </section>
 
-    <section class="col d-flex justify-content-center align-items-stretch" style="width: 50vh; height: 100vh;">
+    <section class="login col d-flex justify-content-center align-items-stretch" style="width: 50vh; height: 100vh;">
 
       <form action="./login_usuario.php" method="post" class="d-flex flex-column justify-content-center align-items-center">
         <h3 class="mb-4 font-weight-bold text-white">ÁREA DO ADMINISTRADOR</h3>
-
-
 
         <input class="mb-3 form-control border border-white" type="text" name="username" placeholder="Digite o seu username">
         <input class="mb-3 form-control border border-white" type="password" name="senha" placeholder="Digite a sua senha">
         <button class="btn btn-outline-light btn-block" type="submit"><b>Entrar</b></button>
       </form>
 
-
-
     </section>
-
 
   </main>
 </body>
