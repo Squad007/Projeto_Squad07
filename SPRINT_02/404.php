779fbd0a8f788706b9f55aa1f1676c91f263dc45
Roger <williroger@usp.br>
2021-01-19 20:04:30 -0300

    404 page

---
+++ SPRINT_02/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php include('./ADM/bootstrap.html') ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+    <title>Sobre Nós</title>
+
+</head>
+
+<body>
+        <!--------------------- MENU ---------------------------->
+        <?php include('./includes/menu.html') ?>
+
+        <main style="min-height: 67vh">
+            <div class="container mb-auto p-5 rounded table-responsive-sm align-items-center justify-content-center animate__animated animate__flash" style="height: 50vh;">
+                    <img src="./img/404.svg" alt="erro 404" width="75%">
+            </div>  
+        </main>
+    
+</body>
+        <!------------------------------- Rodape -->
+        <?php include('./includes/rodape.html') ?>
+
+</html>
