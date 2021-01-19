6c62e77abe9cba732f72260b9e36cebaa444d67c
Roger <williroger@usp.br>
2021-01-19 14:44:11 -0300

    alterações cards

--- SPRINT_02/ADM/pagmed.php
+++ SPRINT_02/ADM/pagmed.php
@@ -45,7 +45,7 @@ background: linear-gradient(180deg, rgba(18,18,20,1) 0%, rgba(44,35,69,1) 100%);
     <hr class="my-4 bg-white">
     <p class="lead font-weight-bold">Atualmente cadastrados:</p>
     <div class="table-responsive">
-    <table class="table table-striped table-hover table-dark bg-dark text-center table-responsive">
+    <table class="table table-striped table-hover table-dark bg-dark text-center">
       <thead>
         <tr>
           <th scope="col" class=" text-left">ID</th>
--- SPRINT_02/index.php
+++ SPRINT_02/index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Home - SOS Medicamentos</title>
   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Bree+Serif&display=swap" rel="stylesheet">
+  <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
   <?php include('./ADM/bootstrap.html') ?>
 
 
@@ -19,7 +20,7 @@
 
   </header>
 
-  <main>
+  <main style="min-height: 68vh">
 
         <div class="jumbotron jumbotron-fluid text-sm-center" style="background: rgba(253, 253, 253, 0);">
           <div class="container">
@@ -37,113 +38,64 @@
 
               </div>
               <div class="col">
-                <img class="float-right" height="100%" src="./img/pill.svg" alt="medicamento">
+                <img class="float-right animate__pulse animate__delay-2s" src="./img/pill.svg" alt="medicamento" style="max-width:310px; min-width: 150px;">
 
               </div>
             </div>
           </div>
         </div>
 
-        <!-- <div class="jumbotron jumbotron-fluid text-sm-center" style="background: rgba(253, 253, 253, 0);">
-          <div class="container">
-            <div class="row justify-content-center align-items-center">
-
-            <div class="col">
-                <img class="float-center" width="250rem" src="./img/mapa_purple.svg" alt="medicamento">
-
-            </div>
-              
-            <div class="col" style="font-family: Bebas Neue; font-size: 40px; color:#00b9b3;">
-                <h1>
-                 Faltou medicamento na Unidade básica de saude (UBS)?
-                </h1>
-                <h1>
-                <a href="./register_notification.php" style="font-family: Bebas Neue; font-size: 40px; color:#6f42c1;">
-                  Notifique a falta
-                <i class="far fa-hand-pointer"></i>
-                </a> 
-                </h1>
-
-              </div>
-            </div>
-          </div>
-        </div> -->
-
-  <!-- <style>
-
-        /* nome das classes => home_ClassNameBootstrap */
-
-    .home_card{
-        background: #00b9b3;
-        border-radius: 15px;
-        border-style: none;
-    }
-
-    .home-btn {
-        width: 100%;
-        background: #6f42c1;
-        font-family: Bebas Neue;
-        font-weight: normal;
-        font-size: 20px;
-        border-style: none;
-    }
-
-    .home-btn:hover {
-        background: #5962e4;
-    }
-
-
-  </style> -->
   <section>
       <div class="container">
         <div class="row">
           
-
-          <div class="col-md-4">
-            <div class="card mb-4 home_card">
-              <div class="card-body"
+          <div class="col-md-4 my-3">
+            <div class="card mb-4 home_card h-100">
+                <div class="card-body"
                   <h4 class="card-title" style="color: #ffffff;font-family: Bebas Neue;font-size: 30px; line-height: 1.2;">ultimas NOTIFICAções </h4>
                  
                   <p class="card-text" 
                   style="color: #ffffff;font-size: 20px; margin-top: 10px;">
-                    O acesso à informação junto com o direito à saúde, são uns dos direitos fundamentais.
+                  Veja as últimas notificações. O acesso à informação é um dos dos direitos fundamentais garantido na nossa Constituição.
                   </p>
-
-                  <button class="btn btn-primary home-btn" type="button">ACESSAR</button>
-              </div>
+                </div>
+                <div class="card-footer">
+                <a class="btn btn-primary home-btn" href="./notification.php" type="button">ACESSAR</a>
+                </div>
             </div>
           </div>
 
 
-          <div class="col-md-4">
-            <div class="card mb-4 home_card">
-              <div class="card-body"
-                  <h4 class="card-title" style="color: #ffffff;font-family: Bebas Neue;font-size: 30px; line-height: 1.2;">Registre sua Notificação</h4>
-                 
-                  <p class="card-text" 
-                  style="color:#ffffff;font-size: 20px; margin-top: 10px;">
-                  Notifique da falta de algum dos medicamento que deveria ser fornecido pelo Sistema Único de Saúde.
-                  </p>
-
-                  <button class="btn btn-primary home-btn" type="button">Notificar</button>
-              </div>
+          <div class="col-md-4 my-3">
+            <div class="card mb-4 home_card h-100">
+                <div class="card-body"
+                    <h4 class="card-title" style="color: #ffffff;font-family: Bebas Neue;font-size: 30px; line-height: 1.2;">Registre sua Notificação</h4>
+                  
+                    <p class="card-text" 
+                    style="color:#ffffff;font-size: 20px; margin-top: 10px;">
+                    Notifique da falta de algum dos medicamento que deveria ser fornecido pelo Sistema Único de Saúde.
+                    </p> 
+                </div>
+                <div class="card-footer">
+                  <a class="btn btn-primary home-btn" href="./register_notification.php" type="button">Notificar</a>
+                </div>
             </div>
           </div>
 
 
-          <div class="col-md-4">
-            <div class="card mb-4 home_card">
-              <div class="card-body"
-                  <h4 class="card-title" style="color: #ffffff;font-family: Bebas Neue;font-size: 30px; line-height: 1.2;">Últimos 30 dias</h4>
+          <div class="col-md-4 my-3">
+            <div class="card mb-4 home_card h-100">
+                <div class="card-body"
+                  <h4 class="card-title" style="color: #ffffff;font-family: Bebas Neue;font-size: 30px; line-height: 1.2;">Sobre  Nós</h4>
                  
                   <p class="card-text" 
                   style="color: #ffffff;font-size: 20px; margin-top: 10px;">
-                  Medicamento com mais notificações de faltas:
-                  UBS com mais notificações de faltas:
-                  </p>
-
-                  <button class="btn btn-primary home-btn" type="button">SOBRE</button>
-              </div>
+                  Somos o Squad 07 (SP1) da Recode Pro 2020. Estamos trabalhando para desenvolver ferramentas que ajude a comunidade.
+                  </p>  
+                </div>
+                <div class="card-footer">
+                  <a class="btn btn-primary home-btn"  href="./contact.php" type="button" >CONTATO</a>
+                </div>
             </div>
           </div>
 
