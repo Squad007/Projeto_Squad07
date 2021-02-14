1af6140505731933708b5d74841bb7550a22836f
Nayara <nayara.lopes2007@gmail.com>
2021-02-14 15:08:57 -0300

    Colocando pag styles

--- SPRINT_03/front_adm/src/Pages/Adms/Adms.jsx
+++ SPRINT_03/front_adm/src/Pages/Adms/Adms.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Style from './styles';
+
 
 export default function Adms () {
     return (
--- SPRINT_03/front_adm/src/Pages/Ubs/Ubs.jsx
+++ SPRINT_03/front_adm/src/Pages/Ubs/Ubs.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Style from './styles';
 
 export default function Ubs() {
     return (
