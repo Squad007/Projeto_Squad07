dfac6dfe96e367a73471531803f03069047e7c07
Nayara <nayara.lopes2007@gmail.com>
2021-02-14 15:20:28 -0300

    Organizando estilos

--- SPRINT_03/front_adm/src/Pages/Adms/Adms.jsx
+++ SPRINT_03/front_adm/src/Pages/Adms/Adms.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
-import Style from './styles';
+import React from "react";
+import Style from "./styles";
 
+export default function Adms() {
+  return (
+    <div style="background: rgb(18,18,20);
+    background: linear-gradient(90deg, rgba(18,18,20,1) 0%, rgba(18,18,20,1) 50%, rgba(83,66,131,1) 100%); color:white;">
+      <main class="d-flex">
+        <section
+          class="col d-flex justify-content-center align-items-center"
+          style="width: 50vh; height: 100vh;"
+        >
+          <div
+            class="logo"
+            style="background-image: url(./img/logo.svg); width: 60%; height: 80vh; background-position: center; background-size:contain; background-repeat:no-repeat"
+          ></div>
+        </section>
 
-export default function Adms () {
-    return (
-        <div>
-            <h1>ADMS</h1>
-        </div>
-    )
+        <section
+          class="login col d-flex justify-content-center align-items-stretch"
+          style="width: 50vh; height: 100vh;"
+        >
+          <form
+            action="./login_usuario.php"
+            method="post"
+            class="d-flex flex-column justify-content-center align-items-center"
+          >
+            <h3 class="mb-4 font-weight-bold text-white">
+              ÁREA DO ADMINISTRADOR
+            </h3>
+
+            <input
+              class="mb-3 form-control border border-white"
+              type="text"
+              name="username"
+              placeholder="Digite o seu username"
+            />
+            <input
+              class="mb-3 form-control border border-white"
+              type="password"
+              name="senha"
+              placeholder="Digite a sua senha"
+            />
+            <button class="btn btn-outline-light btn-block" type="submit">
+              <b>Entrar</b>
+            </button>
+          </form>
+        </section>
+      </main>
+    </div>
+  );
 }
