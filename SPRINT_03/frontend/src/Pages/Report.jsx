15804e020e99f17ee9b5c8e03dfff1f34a5f2201
CristianoSantan <cristiano.mer@gmail.com>
2021-02-20 17:33:49 -0300

    export report

--- SPRINT_03/frontend/src/Pages/Report.jsx
+++ SPRINT_03/frontend/src/Pages/Report.jsx
@@ -5,40 +5,39 @@ export default function Report() {
   const [urlDen, setUrlDen] = useState("http://localhost:3001/den/");
 
   // --------------------------------------- Export report .CSV
-  const objectToCsv = function(data) {
-
+  const objectToCsv = function (data) {
     const csvRows = [];
     // get the headers
     const headers = Object.keys(data[0]);
-    csvRows.push(headers.join(','));
+    csvRows.push(headers.join(","));
 
     // loop over the rows
     for (const row of data) {
-      const values = headers.map(header => {
-        const escape = (''+row[header]).replace(/"/g, '\\"');
+      const values = headers.map((header) => {
+        const escape = ("" + row[header]).replace(/"/g, '\\"');
         return `"${escape}"`;
       });
-      csvRows.push(values.join(','));
+      csvRows.push(values.join(","));
     }
-    return csvRows.join('\n');
-  }
+    return csvRows.join("\n");
+  };
 
-  const download = function(data) {
-    const blob = new Blob([data], { type: 'text/csv' });
+  const download = function (data) {
+    const blob = new Blob([data], { type: "text/csv" });
     const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.setAttribute('hidden', '');
-    a.setAttribute('href', url);
-    a.setAttribute('download', 'SOS_medicamento.csv');
+    const a = document.createElement("a");
+    a.setAttribute("hidden", "");
+    a.setAttribute("href", url);
+    a.setAttribute("download", "SOS_medicamento.csv");
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-  }
+  };
 
   const getReport = async function () {
     const res = await fetch(urlDen);
     const json = await res.json();
-  
+
     const data = json.map((row) => ({
       ubs: row.nomeUbs,
       medicamento: row.nome,
@@ -59,7 +58,7 @@ export default function Report() {
     fetchMyAPI();
   }, [urlDen]);
 
-  // --------------------------------------- Formatação da data 
+  // --------------------------------------- Formatação da data
   const formatDate = (rawDate) => {
     const myDate = new Date(rawDate);
     const d = myDate.getDay();
@@ -87,45 +86,155 @@ export default function Report() {
         <h4 className="border-bottom border-white text-white">RELATÓRIOS</h4>
 
         <div className="text-white text-center flex-column d-flex justify-content-between">
-          <button className="btn" value="1" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/1/2021`)}>JANEIRO</button>
-          <button className="btn" value="2" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/2/2021`)}>FEVEREIRO</button>
-          <button className="btn" value="3" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/3/2021`)}>MARÇO</button>
-          <button className="btn" value="4" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/4/2020`)}>ABRIL</button>
-          <button className="btn" value="5" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/5/2020`)}>MAIO</button>
-          <button className="btn" value="6" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/6/2020`)}>JUNHO</button>
-          <button className="btn" value="7" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/7/2020`)}>JULHO</button>
-          <button className="btn" value="8" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/8/2020`)}>AGOSTO</button>
-          <button className="btn" value="9" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/9/2020`)}>SETEMBRO</button>
-          <button className="btn" value="10" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/10/2020`)}>OUTUBRO</button>
-          <button className="btn" value="11" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/11/2020`)}>NOVEMBRO</button>
-          <button className="btn" value="12" onClick={() => setUrlDen(`http://localhost:3001/den/byMonth/12/2020`)}>DEZEMBRO</button>
+          <button
+            className="btn"
+            value="1"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/1/2021`)
+            }
+          >
+            JANEIRO
+          </button>
+          <button
+            className="btn"
+            value="2"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/2/2021`)
+            }
+          >
+            FEVEREIRO
+          </button>
+          <button
+            className="btn"
+            value="3"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/3/2021`)
+            }
+          >
+            MARÇO
+          </button>
+          <button
+            className="btn"
+            value="4"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/4/2020`)
+            }
+          >
+            ABRIL
+          </button>
+          <button
+            className="btn"
+            value="5"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/5/2020`)
+            }
+          >
+            MAIO
+          </button>
+          <button
+            className="btn"
+            value="6"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/6/2020`)
+            }
+          >
+            JUNHO
+          </button>
+          <button
+            className="btn"
+            value="7"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/7/2020`)
+            }
+          >
+            JULHO
+          </button>
+          <button
+            className="btn"
+            value="8"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/8/2020`)
+            }
+          >
+            AGOSTO
+          </button>
+          <button
+            className="btn"
+            value="9"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/9/2020`)
+            }
+          >
+            SETEMBRO
+          </button>
+          <button
+            className="btn"
+            value="10"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/10/2020`)
+            }
+          >
+            OUTUBRO
+          </button>
+          <button
+            className="btn"
+            value="11"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/11/2020`)
+            }
+          >
+            NOVEMBRO
+          </button>
+          <button
+            className="btn"
+            value="12"
+            onClick={() =>
+              setUrlDen(`http://localhost:3001/den/byMonth/12/2020`)
+            }
+          >
+            DEZEMBRO
+          </button>
         </div>
-        
       </div>
       <div
-        className="relatorio col-5 m-4 p-3 overflow-auto"
+        className="relatorio col-5 m-4 p-3"
         style={{ background: "#00b9b3", borderRadius: "20px", height: "580px" }}
       >
         <div className="">
-        <h4 className="border-bottom border-white text-white text-center pb-2">RELATÓRIO DO MÊS DE </h4>
+          <h4 className="border-bottom border-white text-white text-center pb-2">
+            RELATÓRIO DO MÊS DE{" "}
+          </h4>
         </div>
-
-        {notific.length > 0 && 
+        <div className="h-75 overflow-auto">
+          {notific.length > 0 &&
             notific.map((not, index) => (
               <div key={index} className="m-3 row border-bottom border-white">
                 <div className="col-8 ">
                   <b className="nome_ubs">{not.nomeUbs}</b>
                   <p className="lead font-weight-bold ">{not.nome}</p>
-                  
                 </div>
                 <div className="col-4 justify-content-center flex-column py-auto d-flex align-items-center">
                   <p className="m-0">Data da Falta:</p>
-                  <p style={{fontSize: "25px"}} >{formatDate(not.data_ocorrencia)}</p>
-                  <p style={{fontSize: ""}} >{formatDate(not.data_denuncia)}</p>
+                  <p style={{ fontSize: "25px" }}>
+                    {formatDate(not.data_ocorrencia)}
+                  </p>
+                  <p style={{ fontSize: "" }}>
+                    {formatDate(not.data_denuncia)}
+                  </p>
                 </div>
               </div>
             ))}
-      <button onClick={getReport}>.CSV</button>
+        </div>
+
+        <button style={{
+          background: "#6f42c1",
+          border: "none",
+          borderRadius: "20px",
+          width: "200px",
+          right: "10px",
+          bottom: "10px",
+          position: "absolute",
+        }} onClick={getReport}>.CSV</button>
       </div>
     </div>
   );
