930ac066e7256cac3d7cb68d91d041c0413f2611
matsuboshi <matsuboshi@hotmail.com>
2021-02-21 10:22:51 -0300

    CSS styling on Notification Page

--- SPRINT_03/frontend/src/Pages/Notification.jsx
+++ SPRINT_03/frontend/src/Pages/Notification.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
 import Leaflet from 'leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 import 'leaflet/dist/leaflet.css';
+import 'react-leaflet-markercluster/dist/styles.min.css';
 import '../Assets/css/notification.css';
 import mapMarkerImg from '../Assets/img/pin_sos.svg';
 
@@ -53,30 +55,136 @@ export default function Notification() {
     return num;
   }
   return (
-    <div className="container">
+    // <div className="container">
+    <div>
       <div
-        className="d-flex justify-content-center row"
-        style={{ minHeight: 'calc(100vh - 68px - 56px )' }}
+        className="d-flex flex-row-reverse justify-content-center row overflow-hidden"
+        style={{
+          background: '#00b9b3',
+          margin: '20px',
+          borderRadius: '20px',
+          padding: '0px',
+        }}
       >
-        <div className="col-md-5 my-4">
-          <div
-            className="card_not p-3 text-white anyClass"
-            style={{ background: '#00b9b3' }}
+        <div
+          id="reportMap"
+          className="col-lg-6 m-0 p-0 h-100"
+          style={{
+            height: '100%',
+          }}
+        >
+          <MapContainer
+            center={[-23.49, -46.43]}
+            zoom={13}
+            style={{
+              width: '100%',
+              height: 'calc(100vh - 68px - 56px - 41px )',
+            }}
+            className="card_map"
           >
-            <h4 className="border-bottom border-white">
+            <TileLayer
+              url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+            />
+
+            <MarkerClusterGroup>
+              {pinArray.map((pin, index) => (
+                <Marker
+                  key={index}
+                  icon={mapIcon}
+                  position={[pin.latitude, pin.longitude]}
+                >
+                  <Tooltip>{pin.qtde}</Tooltip>
+                  <Popup
+                    closeButton={false}
+                    minWidth={150}
+                    maxWidth={200}
+                    className="map-popup"
+                  >
+                    <div className="d-flex flex-column justify-content-center align-items-center p-0 m-0">
+                      <b
+                        style={{
+                          fontSize: '15px',
+                          padding: '5px',
+                          textAlign: 'center',
+                        }}
+                      >
+                        {pin.nomeUbs}
+                      </b>
+                      <button
+                        className="btn btn-primary m-0"
+                        onClick={() =>
+                          setUrlDen(`http://localhost:3001/den/byUBS/${pin.id}`)
+                        }
+                      >
+                        Notificações
+                      </button>
+                    </div>
+                  </Popup>
+                </Marker>
+              ))}
+            </MarkerClusterGroup>
+          </MapContainer>
+        </div>
+
+        <div className="p-4 d-lg-none text-white">
+          <i
+            class="fas fa-arrow-alt-circle-up"
+            style={{ fontSize: '1.5rem' }}
+          ></i>
+          <span> </span>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <i className="fas fa-ellipsis-v" style={{ fontSize: '1.5rem' }}></i>
+          <span> </span>
+          <i
+            class="fas fa-arrow-alt-circle-down"
+            style={{ fontSize: '1.5rem' }}
+          ></i>
+        </div>
+
+        <div className="col-lg-6 m-0 p-0 h-100">
+          <div
+            className="card_not p-3 text-white overflow-auto"
+            style={{
+              height: 'calc(100vh - 68px - 56px - 41px )',
+              background: '#00b9b3',
+            }}
+          >
+            <h4 className="font-weight-bold text-center mb-3">
               NOTIFICAÇÕES RECENTES
             </h4>
-            <div className="table-responsive" style={{ height: '90%' }}>
-              <table className="table table-borderless">
+
+            <div className="table-responsive ">
+              <table className="table table-borderless ">
                 <tbody>
                   {notificationButton.length > 0 ? (
                     notificationButton.map((not, index) => (
-                      <tr
-                        key={index}
-                        className="m-3 row border-bottom border-white"
-                      >
-                        <td className="col">
+                      <tr key={index} className="m-3 row">
+                        <td
+                          className="col tabela_ubs_med"
+                          style={{
+                            background: 'rgba(0,0,0,0.2)',
+                            borderRadius: '20px 0px 0px 20px',
+                          }}
+                        >
                           <div>
+                            <b className="nome_ubs d-lg-none">
+                              {formatDate(not.data_ocorrencia)} -{' '}
+                            </b>
                             <b className="nome_ubs">{not.nomeUbs}</b>
                             <p className="lead font-weight-bold text-white ">
                               {not.nome}
@@ -84,10 +192,22 @@ export default function Notification() {
                           </div>
                         </td>
 
-                        <td>
+                        <td
+                          className="tabela_data"
+                          style={{
+                            background: 'rgba(0,0,0,0.2)',
+                            borderRadius: '0px 20px 20px 0px',
+                          }}
+                        >
                           <div className="justify-content-center flex-column py-auto d-flex align-items-center">
                             <p className="m-0 text-white">Data da Falta:</p>
-                            <p style={{ fontSize: '26px', color: '#fff' }}>
+                            <p
+                              style={{
+                                fontSize: '20px',
+                                color: '#fff',
+                                fontWeight: 'bold',
+                              }}
+                            >
                               {formatDate(not.data_ocorrencia)}
                             </p>
                             <p style={{ fontSize: '12px' }}>
@@ -104,45 +224,6 @@ export default function Notification() {
               </table>
             </div>
           </div>
-        </div>
-        <div id="reportMap" className="col-md-7 my-4">
-          <MapContainer
-            center={[-23.49, -46.43]}
-            zoom={13}
-            style={{ width: '100%', height: '550px' }}
-            className="card_map"
-          >
-            {/* <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-            <TileLayer
-              url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-            />
-
-            {pinArray.map((pin, index) => (
-              <Marker
-                key={index}
-                icon={mapIcon}
-                position={[pin.latitude, pin.longitude]}
-              >
-                <Tooltip>{pin.qtde}</Tooltip>
-                <Popup
-                  closeButton={false}
-                  minWidth={240}
-                  maxWidth={240}
-                  className="map-popup"
-                >
-                  <button
-                    className="btn btn-primary"
-                    onClick={() =>
-                      setUrlDen(`http://localhost:3001/den/byUBS/${pin.id}`)
-                    }
-                  >
-                    Notificações
-                  </button>
-                  {pin.nomeUbs}
-                </Popup>
-              </Marker>
-            ))}
-          </MapContainer>
         </div>
       </div>
     </div>
